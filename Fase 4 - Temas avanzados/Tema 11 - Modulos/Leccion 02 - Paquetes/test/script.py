0bfe2e1bccff5460e3695f92b46ca836aa94ee62
eridam <erika.c.caballero@gmail.com>
2018-06-03 11:07:16 +0200

    Crear un directorio test y añadir comentarios al scrpit de test

--- Fase 4 - Temas avanzados/Tema 11 - Modulos/Leccion 02 - Paquetes/test/script.py
+++ Fase 4 - Temas avanzados/Tema 11 - Modulos/Leccion 02 - Paquetes/test/script.py
@@ -12,4 +12,7 @@ saludar() # llamo a la funcion
 # Objeto de tipo Despedida
 Despedida()
 
- 
+# Si ejecutamos lo anterior, nos dará error ModuleNotFoundError: No module named 'paquete'
+# Por que no encuentra el paquete, habría que crear el directorio como en la lección anterior.
+# Tenemos que instalar el paquete dentro d python,  hay que convertir en un paquete distribuible
+# Ver siguiente lección.
